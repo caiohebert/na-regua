5e027048ed5b9e87f05fedd1cf056c01941d31ff
IgorPBorja <igorpradoborja@gmail.com>
2025-11-21 17:30:48 -0300

    initial commit

---
+++ lib/auth_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'auth_provider.g.dart';
+
+@riverpod
+Stream<AuthState> authState(Ref ref) {
+  final supabase = Supabase.instance.client;
+  return supabase.auth.onAuthStateChange;
+}
+
+@riverpod
+class Auth extends _$Auth {
+  @override
+  FutureOr<void> build() {}
+
+  Future<void> signInWithGoogle() async {
+    try {
+      await Supabase.instance.client.auth.signInWithOAuth(OAuthProvider.google);
+    } catch (e) {
+      debugPrint('Error signing in with Google (Supabase): $e');
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await Supabase.instance.client.auth.signOut();
+    } catch (e) {
+      debugPrint('Error signing out: $e');
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'supabase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:na_regua/auth_provider.dart';
+import 'package:na_regua/task/task_list_page.dart';
+
+// Change to true to enable authentication
+// with Google Sign In
+const authenticationEnabled = false;
+
+// Sample supbase_options.dart file:
+//
+// const Map<Symbol, dynamic> supabaseOptions = {
+//   #url: 'https://qwerty.supabase.co',
+//   #anonKey: '1234567890',
+// };
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Function.apply(Supabase.initialize, [], supabaseOptions);
+
+  runApp(
+    const ProviderScope(
+      child: MaterialApp(
+        home: authenticationEnabled ? AuthenticationWrapper() : MainPage(),
+      ),
+    ),
+  );
+}
+
+class MainPage extends ConsumerWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // return const Scaffold(body: Center(child: Text('Hello')));
+    return TaskListPage();
+  }
+}
+
+class AuthenticationWrapper extends ConsumerWidget {
+  const AuthenticationWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authStateAsync = ref.watch(authStateProvider);
+
+    return authStateAsync.when(
+      data: (AuthState state) {
+        return state.session == null ? const SignInPage() : const MainPage();
+      },
+      loading: () =>
+          const Scaffold(body: Center(child: CircularProgressIndicator())),
+      error: (error, __) {
+        return Scaffold(body: Center(child: Text('Error: $error')));
+      },
+    );
+  }
+}
+
+class SignInPage extends ConsumerWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign In')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => ref.read(authProvider.notifier).signInWithGoogle(),
+          child: const Text('Sign in with Google'),
+        ),
+      ),
+    );
+  }
+}
