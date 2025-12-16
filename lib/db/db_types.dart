b4cb85bf9becaf09116338d689f661071a0bea7d
IgorPBorja <igorpradoborja@gmail.com>
2025-12-16 19:26:53 -0300

    fix status in english on UI

--- lib/db/db_types.dart
+++ lib/db/db_types.dart
@@ -2,8 +2,26 @@ enum AppointmentStatus {
   pending,
   confirmed,
   cancelled;
+  // completed  // TODO add completed status later
 
   String get dbName => name.toUpperCase();
+  String get displayName {
+    switch (this) {
+      case AppointmentStatus.pending:
+        return 'PENDENTE';
+      case AppointmentStatus.confirmed:
+        return 'CONFIRMADO';
+      case AppointmentStatus.cancelled:
+        return 'CANCELADO';
+    }
+  }
+
+  static AppointmentStatus fromDbName(String dbName) {
+    return AppointmentStatus.values.firstWhere(
+      (e) => e.dbName == dbName,
+      orElse: () => throw Exception('Unknown appointment status: $dbName'),
+    );
+  }
 }
 
 enum TimeSlotStatus {
